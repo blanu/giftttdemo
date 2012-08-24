706fb71b850ba2461b9be2fc15fb686d23880160
Brandon Wiley <brandon@blanu.net>
2012-08-23 22:34:20 -0500

    Updated to use the new Facebok SDK

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -1,7 +1,7 @@
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : 'YOUR_APP_ID', // App ID
-      channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+      appId      : '248930761793687', // App ID
+      channelUrl : '//blanu.net/gifttt/channel.html', // Channel File
       status     : true, // check login status
       cookie     : true, // enable cookies to allow the server to access the session
       xfbml      : true  // parse XFBML
